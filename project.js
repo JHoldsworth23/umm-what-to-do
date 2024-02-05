09c8734e983fb7a95d728f919668405f83711064
JHoldsworth23 <jacobph101@icloud.com>
2024-02-05 21:15:22 +0000

    Add the  function to display projects on the left panel

--- project.js
+++ project.js
@@ -9,3 +9,40 @@ class Project {
 
 defaultProjects.push(new Project('Gym'));
 defaultProjects.push(new Project('Study'));
+
+const displayProject = (projectArray) => {
+    projectArray.forEach(project => {
+        addProject(project.name);
+    });
+}
+
+const addProject = (projectInput) => {
+    const project = document.querySelector('.left-panel');
+    
+    const projectContainer = document.createElement('div');
+    projectContainer.classList.add('panel');
+
+    const icon = document.createElement('i');
+    icon.setAttribute('class', 'fa-solid fa-list');
+    projectContainer.appendChild(icon);
+
+    const projectName = document.createElement('p');
+    projectName.classList.add('project-name');
+    projectName.textContent = projectInput;
+    projectContainer.appendChild(projectName);
+
+    const editProjectDiv = document.createElement('div');
+    editProjectDiv.classList.add('edit-div');
+    const editIcon = document.createElement('i');
+    editIcon.setAttribute('class', 'fa-regular fa-pen-to-square');
+    const deleteIcon = document.createElement('i');
+    deleteIcon.setAttribute('class', 'fa-solid fa-trash');
+
+    editProjectDiv.appendChild(editIcon);
+    editProjectDiv.appendChild(deleteIcon);
+    projectContainer.appendChild(editProjectDiv);
+
+    project.appendChild(projectContainer);
+}
+
+displayProject(defaultProjects);
