6a8f9308ea04b5926d86a53c3d0d02316b567147
JHoldsworth23 <jacobph101@icloud.com>
2024-02-07 20:26:16 +0000

    Add temporary codes for project form and button from form.js

--- webpage.js
+++ webpage.js
@@ -61,6 +61,7 @@ const createProjectsDiv = () => {
 
     sidebar.appendChild(homeProjects);
     sidebar.appendChild(projectDiv);
+    projectFormButton();
 }
 
 const createFooter = () => {
@@ -75,10 +76,44 @@ const createFooter = () => {
     `;
 }
 
+// IMPORT THESE FUNCTIONS FROM FORM.JS
+const projectFormButton = () => {
+    const projectDiv = document.querySelector('.projects');
+    const projectFormButton = document.createElement('button');
+    projectFormButton.id = 'add-new-project';
+    projectFormButton.innerHTML = '<i class="fa-solid fa-file-circle-plus"></i> Add a New Project';
+    projectDiv.appendChild(projectFormButton);
+}
+
+const createProjectForm = () => {
+    const projectDiv = document.querySelector('.projects');
+    const addNewBtn = document.querySelector('#add-new-project');
+    const form = document.createElement('form');
+    form.id = 'project-form';
+    form.autocomplete = 'off';
+    form.classList.add('hidden');
+
+    const inputDiv = document.createElement('div');
+    inputDiv.classList.add('project-inputs');
+
+    inputDiv.innerHTML = `
+        <input type="text" id="new-project" placeholder="Enter a New Project Name" maxlength="20">
+        <div class="form-buttons">
+            <button class="submit-btn">Add</button>
+            <button class="cancel-btn">Cancel</button>
+        </div>
+    `;
+
+    form.appendChild(inputDiv);
+    projectDiv.insertBefore(form, addNewBtn);
+}
+// 
+
 const loadWebpage = () => {
     createMainComponent();
     createHeader();
     createProjectsDiv();
+    createProjectForm();
     createFooter();
 }
 
