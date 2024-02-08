15b956cfc35d97cf819d1df26bead66da18c8f2a
JHoldsworth23 <jacobph101@icloud.com>
2024-02-08 20:51:42 +0000

    Add the task div to display task

--- task.js
+++ task.js
@@ -9,3 +9,55 @@ class Task {
         this.completed = false;
     }
 }
+
+// const displayTask = (currentProject) => {
+//     const todoDiv = document.querySelector('.todo-list');
+//     todoDiv.textContent = '';
+//     defaultProjects[currentProject].taskList.forEach(task => {
+//         addTask(task.id, task.title, task.details, task.priority, task.dueDate, task.completed);
+//     });
+// }
+
+const addTask = (taskId, taskTitle, details, priority, dueDate, completed) => {
+    const todoDiv = document.querySelector('.todo-list');
+
+    const taskDiv = document.createElement('div');
+    taskDiv.classList.add('task');
+    taskDiv.id = taskId;
+
+    // const checkbox = document.createElement('input');
+    // checkbox.type = 'checkbox';
+    // taskDiv.appendChild(checkbox);
+
+    const priorityStatus = document.createElement('p');
+    priorityStatus.setAttribute('class', `${priority.toLowerCase()}`);
+    priorityStatus.textContent = priority;
+    taskDiv.appendChild(priorityStatus);
+
+    const title = document.createElement('p');
+    title.classList.add('task-title');
+    title.textContent = taskTitle;
+    taskDiv.appendChild(title);
+
+    const detailsBtn = document.createElement('button');
+    detailsBtn.classList.add('details');
+    detailsBtn.textContent = 'Details';
+    taskDiv.appendChild(detailsBtn);
+
+    const date = document.createElement('p');
+    date.classList.add('date');
+    date.textContent = dueDate;
+    taskDiv.appendChild(date);
+
+    const editIcon = document.createElement('i');
+    editIcon.setAttribute('class', 'task-icon fa-regular fa-pen-to-square');
+    taskDiv.appendChild(editIcon);
+
+    const deleteIcon = document.createElement('i');
+    deleteIcon.setAttribute('class', 'task-icon fa-solid fa-trash');
+    taskDiv.appendChild(deleteIcon);
+
+    todoDiv.appendChild(taskDiv);
+}
+
+addTask(0, 'Lift Weights', '', 'Medium', '6th Feb', false);
