1b513eb27b0fa739392e6212af931f9ebde605ac
JHoldsworth23 <jacobph101@icloud.com>
2024-02-26 20:20:49 +0000

    Add functions to display task in a week time and check if it is in next week

--- src/homePanels.js
+++ src/homePanels.js
@@ -1,12 +1,12 @@
-import { format } from "date-fns";
+import { format, isWithinInterval } from "date-fns";
 import { defaultProjects } from "./project";
 import { addTask } from "./task";
 import { findFormattedDate } from "./editTask";
 
 const selectedHomePanel = (homePanel) => {
     if (homePanel.matches('#all-tasks')) displayAllTasks();
-    if (homePanel.matches('#today-tasks')) todayTasks();
-    if (homePanel.matches('#seven-days')) console.log('Display Week Tasks');
+    if (homePanel.matches('#today-tasks')) displayTodayTasks();
+    if (homePanel.matches('#seven-days')) displayWeekTasks();
     if (homePanel.matches('#overdue-tasks')) console.log('Display Overdue Tasks');
     if (homePanel.matches('#high-priority')) console.log('Display Important Tasks');
 }
@@ -25,7 +25,7 @@ const displayAllTasks = () => {
     });
 }
 
-const todayTasks = () => {
+const displayTodayTasks = () => {
     clearList();
     const today = format(new Date(), 'yyyy-MM-dd');
     defaultProjects.forEach(project => {
@@ -37,6 +37,26 @@ const todayTasks = () => {
             }
         });
     });
+}
+
+const displayWeekTasks = () => {
+    clearList();
+    defaultProjects.forEach(project => {
+        project.taskList.forEach(task => {
+            const date = findFormattedDate(task.dueDate);
+            if (checkNextWeek(date)) {
+                addTask(task.id, task.title, task.details, task.priority, task.dueDate);
+            } else {
+                return;
+            }
+        });
+    });
+}
+
+const checkNextWeek = (taskDate) => {
+    const today = new Date();
+    const nextWeekDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 7);
+    return isWithinInterval(taskDate, {start: today, end: nextWeekDate});
 }
 
 export { selectedHomePanel };
