0d9c531db702207ea4d01659af7125918af5145a
JHoldsworth23 <jacobph101@icloud.com>
2024-02-26 20:01:16 +0000

    Add the function to display tasks that is today

--- src/homePanels.js
+++ src/homePanels.js
@@ -1,9 +1,11 @@
-import { defaultProjects, hideAddTaskBtn } from "./project";
+import { format } from "date-fns";
+import { defaultProjects } from "./project";
 import { addTask } from "./task";
+import { findFormattedDate } from "./editTask";
 
 const selectedHomePanel = (homePanel) => {
     if (homePanel.matches('#all-tasks')) displayAllTasks();
-    if (homePanel.matches('#today-tasks')) console.log('Display Today Tasks');
+    if (homePanel.matches('#today-tasks')) todayTasks();
     if (homePanel.matches('#seven-days')) console.log('Display Week Tasks');
     if (homePanel.matches('#overdue-tasks')) console.log('Display Overdue Tasks');
     if (homePanel.matches('#high-priority')) console.log('Display Important Tasks');
@@ -19,6 +21,20 @@ const displayAllTasks = () => {
     defaultProjects.forEach(project => {
         project.taskList.forEach(task => {
             addTask(task.id, task.title, task.details, task.priority, task.dueDate);
+        });
+    });
+}
+
+const todayTasks = () => {
+    clearList();
+    const today = format(new Date(), 'yyyy-MM-dd');
+    defaultProjects.forEach(project => {
+        project.taskList.forEach(task => {
+            if (findFormattedDate(task.dueDate) === today) {
+                addTask(task.id, task.title, task.details, task.priority, task.dueDate);
+            } else {
+                return;
+            }
         });
     });
 }
