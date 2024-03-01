4aa46e7f3f9f5e7ccfd827dda4912d9e53bdda96
JHoldsworth23 <jacobph101@icloud.com>
2024-03-01 21:52:11 +0000

    Change the error message

--- src/editProject.js
+++ src/editProject.js
@@ -31,7 +31,7 @@ const renameProjectForm = () => {
         <div>
             <i class="fa-solid fa-list"></i>
             <input type="text" id="rename-project" maxlength="20">
-            <p class="rename-error hidden">You can't make the project title to be blank</p>
+            <p class="rename-error hidden">You can't leave the blank project title</p>
         </div>
         <div class="rename-buttons">
             <input type="button" class="rename-btn" value="Rename">
